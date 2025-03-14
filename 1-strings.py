46116783c5edd9cc815c31b08514f4b448c34212
mahamed-rashid <mahamed.rashid@gmail.com>
2025-03-14 17:03:52 -0400

    practiced strings

---
+++ 1-strings.py
@@ -0,0 +1,15 @@
+course = "Python for Beginners"
+#len function returns the number of characters in a string
+print(len(course))
+#strings are zero-based index in python
+print(course[0])
+#negative index
+print(course[-1])
+#slicing it returns a new string with the characters in the specified range. starts from index 0 and ends at 2 (3-1)
+print(course[0:3])
+#the below code prints the entire string
+print(course[0:])
+#the below code prints the pyt. by default, the start index is 0
+print(course[:3])
+#the below code prints the entire string
+print(course[:])
