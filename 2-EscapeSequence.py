53ed2b9ed5e6b975eea461e106c28ffd64817d0d
mahamed-rashid <mahamed.rashid@gmail.com>
2025-03-14 17:17:34 -0400

    escpsq

---
+++ 2-EscapeSequence.py
@@ -0,0 +1,21 @@
+#escape sequence \" to display double quote
+escape_seq1 = "I am 6'2\" tall."
+print(escape_seq1)
+#escape sequence \' to display single quote
+escape_seq2 = "Python is easy to \'learn. It's fun."
+print(escape_seq2)
+#escape sequence \\ to display backslash
+escape_seq3 = "This is a backslash: \\"
+print(escape_seq3)
+#escape sequence \n to display new line
+escape_seq4 = "This is a new \nline"
+print(escape_seq4)
+#escape sequence \t to display tab
+escape_seq5 = "This is a tab \tcharacter"
+print(escape_seq5)
+#escape sequence \b to display backspace
+escape_seq6 = "This is a backspace\b character"
+print(escape_seq6)
+#escape sequence \r to display carriage return
+escape_seq7 = "This is a carriage return\r character"
+print(escape_seq7)
